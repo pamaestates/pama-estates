e37d94b462e5c29d5754c86ee3f914fd312a70cc
Patryk <ptybura@gmail.com>
2026-04-18 00:48:29 +0400

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://pamaestates.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://pamaestates.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://pamaestates.com/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://pamaestates.com/areas",
+      lastModified: new Date(),
+    },
+  ];
+}
